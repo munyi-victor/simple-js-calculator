d69cdec14d075550f3b385bd1860d9f3ed46f416
munyi-victor <munyivictor6@gmail.com>
2024-03-02 13:26:31 +0300

    modified functionality

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
 let display = document.getElementById("display");
 
+let answerDisplayed = false;
+
 function toDisplay(value) {
+  if (answerDisplayed && "1234567890".includes(value)) {
+    display.value = "";
+    answerDisplayed = false;
+  }
   display.value += value;
 }
 
 function clearDisplay() {
   display.value = "";
+  answerDisplayed = false;
 }
 
 function calculate() {
   try {
     display.value = eval(display.value);
+    answerDisplayed = true;
   } catch (error) {
     display.value = error;
   }
